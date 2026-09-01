dc16daedb58fb2c244959d743ce7d6dcd7075dd8
Bedant sharma <manurishi1103@gmail.com>
2026-09-01 22:52:11 +0530

    another problem solved.

---
+++ Solutions/PWLFCINSAL.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
+        sfc = [(set(x),i)for i,x in enumerate(favoriteCompanies)]
+        n = len(favoriteCompanies)
+        answer = []
+        sfc.sort(key=lambda x: len(x[0]))
+        for i in range(n):
+            elei = sfc[i][0]
+            flag = True
+            for j in range(i+1,n):
+                elej = sfc[j][0]
+                if elei.issubset(elej):
+                    flag = False
+                    break
+            if flag:
+                answer.append(sfc[i][1])
+        answer.sort()
+        return answer
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    favoriteCompanies = [["leetcode"],["google"],["facebook"],["amazon"]]
+    ans = sol.peopleIndexes(favoriteCompanies)
+    print(ans)
